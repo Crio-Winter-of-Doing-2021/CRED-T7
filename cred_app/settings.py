f353abf0cd10bc2ec8c9cf78d0964ee06a54e405
Shantanu Singh <39329252+CasualCoder99@users.noreply.github.com>
2021-03-02 01:42:05 +0530

    Update settings.py

--- cred_app/settings.py
+++ cred_app/settings.py
@@ -19,8 +19,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xf$p!*#c-17j5+7&$_&#azplg9s8__9y2+81z(96k=#0vt(dlq'
+
+
+import os
+key= ''.join(random.choices(string.ascii_letters+string.digits,k=50))
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', key)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
