6aeea43bb07535bb050c02a724e4a8c24ddde1ab
Shantanu Singh <theshantanusingh@gmail.com>
2021-03-01 23:53:05 +0530

    Django setup and authentication setup

---
+++ accounts/api.py
@@ -0,0 +1,42 @@
+from rest_framework import permissions, generics
+from rest_framework.response import Response
+from knox.models import AuthToken
+from .serializers import UserSerializer, SignUpSerializer, LoginSerializer
+
+
+# SignUp api
+class SignUpAPI(generics.GenericAPIView):
+    serializer_class = SignUpSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+# login API
+class LoginAPI(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            'user': UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": AuthToken.objects.create(user)[1]
+        })
+
+
+# get the user api
+class UserAPI(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
