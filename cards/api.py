badb247b2e869acb62ab14f4e4865968ebc2f599
Aditya Mahajan <aditya.m.1997@gmail.com>
2021-03-06 00:42:12 +0530

    Added Transaction model and url routes

--- cards/api.py
+++ cards/api.py
@@ -1,7 +1,8 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from .serializers import CardSerializer
-from .models import Cards
+from .serializers import CardSerializer, TransactionSerializer
+from .models import Cards, Transactions
+from rest_framework.views import APIView
 
 
 class addCard(generics.ListCreateAPIView):
@@ -18,3 +19,32 @@ class addCard(generics.ListCreateAPIView):
 
     def get_queryset(self):
         return Cards.objects.all()
+
+
+class viewCard(APIView):
+    def get_object(self, pk):
+        try:
+            return Cards.objects.get(pk=pk)
+        except Cards.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        serializer = CardSerializer(snippet)
+        return Response(serializer.data)
+
+
+class addTransaction(generics.ListCreateAPIView):
+    serializer_class = TransactionSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        transaction = serializer.save()
+        return Response({
+            'transaction': TransactionSerializer(transaction, context=self.get_serializer_context()).data,
+        })
+
+    def get_queryset(self):
+        return Transactions.objects.all()
--- cards/apps.py
+++ cards/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class CardsConfig(AppConfig):
     name = 'cards'
+
+class TransactionsConfig(AppConfig):
+    name = 'transactons'
