e44242f252ac0517cd345ddc8a763719aa2a12f7
shantanusingh1069 <shantanusingh1069@gmail.com>
2021-03-07 02:03:02 +0530

    Authentication for cards and statements with payment feature

--- cards/api.py
+++ cards/api.py
@@ -1,32 +1,52 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, serializers
 from rest_framework.response import Response
-from .serializers import CardSerializer, TransactionSerializer
+from .serializers import CardSerializer, TransactionSerializer, payCardSerializer
 from .models import Cards, Transactions
 from rest_framework.views import APIView
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+
+class IsOwnerOrNot(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        print(obj.owner, request.user)
+        return obj.owner == request.user
+
+
+class TransactionIsOwnerOrNot(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner.owner == request.user
 
 
 class addCard(generics.ListCreateAPIView):
     serializer_class = CardSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrNot]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        card = serializer.save()
+        card = serializer.save(owner=self.request.user)
         return Response({
             'card': CardSerializer(card, context=self.get_serializer_context()).data,
         })
 
     def get_queryset(self):
-        return Cards.objects.all()
+        return self.request.user.cards.all()
 
 
-class viewCard(APIView):
+class viewCard(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrNot]
+
     def get_object(self, pk):
         try:
-            return Cards.objects.get(pk=pk)
+            card = Cards.objects.get(pk=pk)
+            if(card.owner == self.request.user):
+                return card
         except Cards.DoesNotExist:
             raise Http404
+        raise Http404
 
     def get(self, request, pk, format=None):
         snippet = self.get_object(pk)
@@ -36,15 +56,38 @@ class viewCard(APIView):
 
 class addTransaction(generics.ListCreateAPIView):
     serializer_class = TransactionSerializer
-    # permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, TransactionIsOwnerOrNot]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        transaction = serializer.save()
+        card = get_object_or_404(self.request.user.cards, pk=self.kwargs['pk'])
+        transaction = serializer.save(owner=card)
+        card.credit += serializer.validated_data['amount']
+        card.save()
+        print(card.credit, serializer.validated_data['amount'])
         return Response({
             'transaction': TransactionSerializer(transaction, context=self.get_serializer_context()).data,
         })
 
     def get_queryset(self):
-        return Transactions.objects.all()
+        # print(self.kwargs['month'], self.kwargs['year'])
+        card = get_object_or_404(self.request.user.cards, pk=self.kwargs['pk'])
+        # return Transactions.objects.all()
+        return card.transactions.all()
+
+
+class payCard(generics.CreateAPIView):
+    serializer_class = payCardSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        card = get_object_or_404(request.user.cards.all(), pk=kwargs['pk'])
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if(serializer.validated_data['pay_amount'] > card.credit):
+            raise serializers.ValidationError(
+                'The payment should be less than or equal to what is to be paid.')
+        card.credit -= serializer.validated_data['pay_amount']
+        card.save()
+        return Response({'Payment Billed': serializer.validated_data['pay_amount']})
--- cards/models.py
+++ cards/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError 
+from django.core.exceptions import ValidationError
 from datetime import datetime
+from django.core.validators import MinValueValidator
+
 
 class Cards(models.Model):
     bank = models.CharField(max_length=200)
@@ -10,30 +12,37 @@ class Cards(models.Model):
     cvv = models.CharField(max_length=3)
     expiry_date_month = models.CharField(max_length=2)
     expiry_date_year = models.CharField(max_length=4)
-    # owner = models.ForeignKey(
-    #     User, on_delete=models.CASCADE, related_name="cards")
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="cards", null=True)
+    credit = models.DecimalField(
+        max_digits=19, decimal_places=2, auto_created=True, default=0.00,
+        validators=[MinValueValidator(0.00)])
 
 
-CREDIT= (
-        ('Credit', 'Credit'),
-        ('Debit', 'Debit'),
-    )
-CAT= (
-        ('Medical', 'Medical'),
-        ('Groceries', 'Groceries'),
-        ('Food', 'Food'),
-        ('Entertainment', 'Entertainment'),
-        ('Bills', 'Bills'),
-        ('Others', 'Others'),
-    )
+CREDIT = (
+    ('Credit', 'Credit'),
+    ('Debit', 'Debit'),
+)
+CAT = (
+    ('Medical', 'Medical'),
+    ('Groceries', 'Groceries'),
+    ('Food', 'Food'),
+    ('Entertainment', 'Entertainment'),
+    ('Bills', 'Bills'),
+    ('Others', 'Others'),
+)
+
 
 class Transactions(models.Model):
-    amount = models.DecimalField(max_digits=19, decimal_places=4)
+    amount = models.DecimalField(max_digits=19, decimal_places=2, validators=[
+                                 MinValueValidator(float('0.01'))])
     vendor = models.CharField(max_length=200)
     CreditorDebit = models.CharField(max_length=200, choices=CREDIT)
-    category = models.CharField(max_length=200,choices=CAT)
+    category = models.CharField(max_length=200, choices=CAT)
     cur_date = datetime.today().date()
-    month = models.CharField(max_length=2,editable=False,default=str(cur_date)[5:7])
-    year = models.CharField(max_length=4,editable=False,default=str(cur_date)[0:4])
-    # owner = models.ForeignKey(
-    #     Cards, on_delete=models.CASCADE, related_name="transactions")
+    month = models.CharField(
+        max_length=2, editable=False, default=str(cur_date)[5:7])
+    year = models.CharField(max_length=4, editable=False,
+                            default=str(cur_date)[0:4])
+    owner = models.ForeignKey(
+        Cards, on_delete=models.CASCADE, related_name="transactions", null=True)
