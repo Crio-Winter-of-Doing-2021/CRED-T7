638e29d3e13d5ddc78be150dc8871575b71a0153
Aditya Mahajan <aditya.m.1997@gmail.com>
2021-03-20 15:39:42 +0530

    Integrated Postgres

--- cred_app/settings.py
+++ cred_app/settings.py
@@ -82,10 +82,20 @@ WSGI_APPLICATION = 'cred_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cred_app',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5433,
     }
 }
 
